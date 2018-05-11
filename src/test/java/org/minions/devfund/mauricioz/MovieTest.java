445a35c6b737669e9211bc32be6fd6071ab113d9
Mauricio <mauricio.zelaya@jalasoft.com>
2018-05-11 12:37:06 -0700

    increassing code coverage and refactoring some methods

--- src/test/java/org/minions/devfund/mauricioz/MovieTest.java
+++ src/test/java/org/minions/devfund/mauricioz/MovieTest.java
@@ -3,6 +3,9 @@ package org.minions.devfund.mauricioz;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -47,5 +50,18 @@ public class MovieTest {
         final double newRating = 8.3;
         movie.setRating(newRating);
         assertEquals(newRating, movie.getRating());
+    }
+
+    /**
+     * test getters and setters for actors
+     */
+    @Test
+    public void testactorsGetSet() {
+        Actor actor = new Actor();
+        actor.setName("actor name");
+        List<Actor> actors = new ArrayList <>();
+        actors.add(actor);
+        movie.setActors(actors);
+        assertEquals(actor.getName(), movie.getActors().get(0).getName());
     }
 }
